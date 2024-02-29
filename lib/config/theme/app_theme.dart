ec17c22b80740bfabb599659082501d1c237d90a
LuisPg10 <dapin46@hotmail.com>
2024-02-29 06:01:06 -0500

    Filled button theme changed

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
+  ThemeData getTheme() => ThemeData(
 
-  ThemeData getTheme() => ThemeData();
+    filledButtonTheme: FilledButtonThemeData(
+      style: ButtonStyle(
+        textStyle: MaterialStatePropertyAll(
+          GoogleFonts.montserrat().copyWith(
+            fontWeight: FontWeight.bold,
+          )
+        )
+      )
+    )
+  );
 }
