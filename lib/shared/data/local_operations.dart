7098c39f26885817c9d9fad01658ea13f1f1214a
LuisPg10 <dapin46@hotmail.com>
2024-02-29 06:41:25 -0500

    like data is

---
+++ lib/shared/data/local_operations.dart
@@ -0,0 +1,21 @@
+List<Map<String, dynamic>> operations = [
+  {
+    "name": "Tasa de interés",
+    "logo": "assets/images/tasa-interes.png"
+  },
+
+  {
+    "name": "Interés simple",
+    "logo": "assets/images/interes-simple.png"
+  },
+
+  {
+    "name": "Interés compuesto",
+    "logo": "assets/images/interes-compuesto.png"
+  },
+
+  {
+    "name": "Anualidades",
+    "logo": "assets/images/anualidades.png"
+  },
+];
