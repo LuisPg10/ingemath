d5729fa8d2e9f1b22b6e3c4f0f8cfe426afd3736
LuisPg10 <dapin46@hotmail.com>
2024-03-03 15:53:39 -0500

    Logo and home screen improved

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:ingemath/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -11,18 +13,43 @@ class HomeScreen extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const IngeMathLogo(size: 150),
-          
-          const Text("IngeMath"),
-          const Text("Money"),
 
-          const SizedBox(height: 150),
-          
-          CustomFilledButton(
-            buttonColor: const Color(0xFFFF833D),
-            onPressed: () => context.push("/operations"),
-            child: const Text("Ingresar", style: TextStyle(fontSize: 24)),
+          const Spacer(flex: 5),
+
+          const IngeMathLogo(size: 150),
+
+          Text("INGEMATH",
+            style: GoogleFonts.squadaOne().copyWith(
+              fontSize: 60,
+              fontWeight: FontWeight.normal,
+              color: const Color(0xFFFF833D),
+              letterSpacing: 10,
+              height: 0
+            ),
           ),
+
+          Text("MONEY",
+            style: GoogleFonts.squadaOne().copyWith(
+              fontSize: 30,
+              fontWeight: FontWeight.normal,
+              color: const Color(0xFF7ED957),
+              letterSpacing: 3,
+              height: 0
+            ),
+          ),
+
+          const Spacer(flex: 2),
+
+          SizedBox(
+            height: 50,
+            child: CustomFilledButton(
+              buttonColor: const Color(0xFFFF833D),
+              onPressed: () => context.push("/operations"),
+              child: const Text("Ingresar", style: TextStyle(fontSize: 24)),
+            ),
+          ),
+
+          const Spacer(flex: 2),
         ],
       ),
     );
