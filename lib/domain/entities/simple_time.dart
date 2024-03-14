31922595e2e69bf9a7e57d43468982b4c828c051
Santiago <juniorsantiagoalvarez03@gmail.com>
2024-03-14 17:32:59 -0500

    Adding TimeResult Entity

---
+++ lib/domain/entities/simple_time.dart
@@ -0,0 +1,13 @@
+class TimeResult {
+  final int years;
+  final int months;
+  final int weeks;
+  final int days;
+
+  TimeResult({
+    required this.years,
+    required this.months,
+    required this.weeks,
+    required this.days,
+  });
+}
