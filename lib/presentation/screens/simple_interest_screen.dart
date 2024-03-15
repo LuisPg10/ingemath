ed1d2b8ec689ca98c2655eeea0d360aeef94bb6a
Santiago <juniorsantiagoalvarez03@gmail.com>
2024-03-14 19:57:00 -0500

    Adding Shared Widget CustomTimeFormFild

--- lib/presentation/screens/simple_interest_screen.dart
+++ lib/presentation/screens/simple_interest_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-// Importa los widgets y providers necesarios
 import 'package:ingemath/presentation/providers/providers.dart';
 import 'package:ingemath/presentation/widgets/widgets.dart';
+import '../widgets/shared/custom_time_form_field.dart';
 
 class SimpleInterestScreen extends StatelessWidget {
   const SimpleInterestScreen({super.key});
@@ -139,98 +138,10 @@ class _SimpleInterestForm extends ConsumerWidget {
 
             const SizedBox(height: 15),
 
-            CustomTextFormField(
-              showIcon: true,
-              icon: Icons.calendar_today,
-              enable: simpleInterestForm.variable != keyOptions[3],
-              label: "Tiempo (Años)",
-              controller: TextEditingController(
-                text: simpleInterestForm.time.value.toStringAsFixed(3),
-              ),
-              errorMessage: simpleInterestForm.isFormPosted &&
-                      simpleInterestForm.variable != SimpleVariable.time
-                  ? simpleInterestForm.time.errorMessage
-                  : null,
-              onIconPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    TextEditingController daysController =
-                        TextEditingController();
-                    TextEditingController weeksController =
-                        TextEditingController();
-                    TextEditingController monthsController =
-                        TextEditingController();
-                    TextEditingController yearsController =
-                        TextEditingController();
-
-                    return AlertDialog(
-                      title: const Text("Establecer Tiempo"),
-                      content: Form(
-                        child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TextFormField(
-                              controller: yearsController,
-                              decoration:
-                                  const InputDecoration(labelText: 'Años'),
-                              keyboardType: TextInputType.number,
-                            ),
-                            TextFormField(
-                              controller: monthsController,
-                              decoration:
-                                  const InputDecoration(labelText: 'Meses'),
-                              keyboardType: TextInputType.number,
-                            ),
-                            TextFormField(
-                              controller: weeksController,
-                              decoration:
-                                  const InputDecoration(labelText: 'Semanas'),
-                              keyboardType: TextInputType.number,
-                            ),
-                            TextFormField(
-                              controller: daysController,
-                              decoration:
-                                  const InputDecoration(labelText: 'Días'),
-                              keyboardType: TextInputType.number,
-                            ),
-                            const SizedBox(height: 20),
-                            SizedBox(
-                              width: double.infinity,
-                              child: CustomFilledButton(
-                                onPressed: () {
-                                  double days =
-                                      double.tryParse(daysController.text) ?? 0;
-                                  double weeks =
-                                      double.tryParse(weeksController.text) ??
-                                          0;
-                                  double months =
-                                      double.tryParse(monthsController.text) ??
-                                          0;
-                                  double years =
-                                      double.tryParse(yearsController.text) ??
-                                          0;
-
-                                  years += (days / 360);
-                                  years += (weeks / 52);
-                                  years += (months / 12);
-
-                                  Navigator.of(context).pop();
-
-                                  ref
-                                      .read(simpleFormProvider.notifier)
-                                      .onTimeChanged(years);
-                                },
-                                child: const Text("Establecer"),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
+            CustomTimeFormField(
+              simpleInterestForm: simpleInterestForm,
+              keyOptions: keyOptions,
+              ref: ref,
             ),
 
             const SizedBox(height: 30),
